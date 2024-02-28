8b22b3c95e902d8e4a5884db17b1a37b6c882464
thseogns <thseo1758@naver.com>
2024-02-29 08:03:59 +0900

    feat:firebase데이터 get, post초안

--- src/App.tsx
+++ src/App.tsx
@@ -11,11 +11,20 @@ import { Routes, Route } from "react-router-dom";
 import AddAlbum from "./components/AddAlbum";
 import AlbumList from "./components/album/AlbumList";
 import Album from "./components/album/Album";
-
+import axios from "axios";
 function App() {
+  const [data, setData] = React.useState("");
+
   const albumNames = useSelector(
     (state: RootState) => state.albumName.albumNames
   );
+
+  const fatch = axios
+    .get("https://photoalbumdb-default-rtdb.firebaseio.com/name.json")
+    .then((res) => setData(res.data))
+    .catch((error) => error);
+  console.log(data);
+
   return (
     <div className="App">
       <AddAlbum />
--- src/features/albumNameSlice.ts
+++ src/features/albumNameSlice.ts
@@ -1,18 +1,45 @@
 /** @format */
 
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
-
+import axios from "axios";
 export interface AlbumState {
   albumNames: string[];
   images: { alt: string; image: { imgName: string; date: string } }[];
+  isloading: boolean;
 }
 
 const initialState: AlbumState = {
   albumNames: [],
   images: [{ alt: "", image: { imgName: "", date: "" } }],
+  isloading: true,
 };
 
+//내에서 작동함.
+export const fetchImage = createAsyncThunk(
+  "albumNameSlice/fetchImage",
+  async () => {
+    return axios
+      .get("https://photoalbumdb-default-rtdb.firebaseio.com/")
+      .then((res) => res.data)
+      .catch((error) => error);
+  }
+);
+
+//디스패치로 전달해준다.
+export const postImageName = createAsyncThunk(
+  "albumNameSlice/postImage",
+  async (_, { getState }) => {
+    const currentState = (getState() as { album: AlbumState }).album;
+    return axios.post(
+      "https://photoalbumdb-default-rtdb.firebaseio.com/images.json",
+      {
+        name: currentState.albumNames,
+        images: currentState.images,
+      }
+    );
+  }
+);
 export const albumNameSlice = createSlice({
   name: "album",
   initialState,
@@ -33,6 +60,17 @@ export const albumNameSlice = createSlice({
     deleteAlbumName: (state, action: PayloadAction<string[]>) => {
       state.albumNames = action.payload;
     },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchImage.pending, (state, action) => {
+      state.isloading = true;
+    });
+    builder.addCase(fetchImage.fulfilled, (state, action) => {
+      state.isloading = false;
+    });
+    builder.addCase(fetchImage.rejected, (state, action) => {
+      state.isloading = false;
+    });
   },
 });
 //액션 사용용도
