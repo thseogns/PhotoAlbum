857e5240b0ccaa2d94761ce8cd0136ab6fe7f9cc
thseogns <thseo1758@naver.com>
2024-02-28 17:26:29 +0900

    feat: 사진정보 변수 생성, 저장

--- src/components/album/Album.tsx
+++ src/components/album/Album.tsx
@@ -3,17 +3,39 @@
 import React from "react";
 import { v4 as uuidv4 } from "uuid"; //아이디를 생성해서 저장하기
 import ImageBox from "./ImageBox";
-
+import type { RootState } from "../../app/store";
+import { useDispatch } from "react-redux";
+import { updateImage } from "../../features/albumNameSlice";
 const Album = ({ albumName }: { albumName: string }) => {
+  const dispatch = useDispatch();
   const [selectedFile, setSelectedFile] = React.useState<File | null>(null);
-
+  const [albums, setAlbums] = React.useState("");
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files !== null) {
       //널값이 아닐때만 작동
       const file = event.target.files[0];
       setSelectedFile(file);
-      const uniqueFileName = `${uuidv4()}-${file.name}`; //같은 이미지저장을 위한 랜덤네임 지정
+      //이름생성하기
+      const name = uuidv4();
+      const uniqueFileName = `${name}-${file.name}`; //같은 이미지저장을 위한 랜덤네임 지정
       console.log(uniqueFileName);
+
+      //업로드 시간 날짜 정하기.
+
+      const year = String(new Date().getFullYear()).padStart(2, "0"); // 년
+      const month = String(new Date().getMonth() + 1).padStart(2, "0"); // 월 (0부터 시작하므로 +1 해줌)
+      const day = String(new Date().getDate()).padStart(2, "0"); // 일
+      const hour = String(new Date().getHours()).padStart(2, "0"); // 시
+      const minute = String(new Date().getMinutes()).padStart(2, "0"); // 분
+      const second = String(new Date().getSeconds()).padStart(2, "0"); // 초
+      const isDate = `${year}-${month}-${day} T${hour}:${minute}:${second}`;
+
+      // 객체를 만들어서 업데이트 해주자. 저장날짜시간, 이름, 앨범이름
+      const updateAlbum = {
+        alt: name,
+        image: { imgName: uniqueFileName, date: isDate },
+      };
+      dispatch(updateImage(updateAlbum));
     }
   };
 
--- src/features/albumNameSlice.ts
+++ src/features/albumNameSlice.ts
@@ -5,12 +5,12 @@ import type { PayloadAction } from "@reduxjs/toolkit";
 
 export interface AlbumState {
   albumNames: string[];
-  images: { image: { imgName: string; date: string } }[];
+  images: { alt: string; image: { imgName: string; date: string } }[];
 }
 
 const initialState: AlbumState = {
   albumNames: [],
-  images: [{ image: { imgName: "", date: "" } }],
+  images: [{ alt: "", image: { imgName: "", date: "" } }],
 };
 
 export const albumNameSlice = createSlice({
@@ -20,12 +20,22 @@ export const albumNameSlice = createSlice({
     albumName: (state, action: PayloadAction<string>) => {
       state.albumNames = [action.payload, ...state.albumNames];
     },
+    updateImage: (
+      state,
+      action: PayloadAction<{
+        alt: string;
+        image: { imgName: string; date: string };
+      }>
+    ) => {
+      state.images = [action.payload, ...state.images];
+    },
     deleteAlbumName: (state, action: PayloadAction<string[]>) => {
       state.albumNames = action.payload;
     },
   },
 });
 //액션 사용용도
-export const { albumName, deleteAlbumName } = albumNameSlice.actions;
+export const { albumName, deleteAlbumName, updateImage } =
+  albumNameSlice.actions;
 //스토어로 보냄
 export default albumNameSlice.reducer;
